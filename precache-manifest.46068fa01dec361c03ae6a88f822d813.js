565202ddb03d94857e4fb6a6c186e0af2b999816
marc.blumberg <marcaugustblumberg@gmail.com>
2020-05-01 07:41:21 -0700

    Updates

---
+++ precache-manifest.46068fa01dec361c03ae6a88f822d813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2f4698f5f92c624114760caa71459e",
+    "url": "/bip/index.html"
+  },
+  {
+    "revision": "4f3f06218fcd043f156a",
+    "url": "/bip/static/css/main.ad3024e6.chunk.css"
+  },
+  {
+    "revision": "65177dcfaedefebb9f2e",
+    "url": "/bip/static/js/2.91c0078a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bip/static/js/2.91c0078a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f3f06218fcd043f156a",
+    "url": "/bip/static/js/main.5c7b752f.chunk.js"
+  },
+  {
+    "revision": "3b5374332049e8faf8c7",
+    "url": "/bip/static/js/runtime-main.87c39fff.js"
+  }
+]);
